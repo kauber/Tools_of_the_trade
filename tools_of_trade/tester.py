55fbcbc6b8fac721b5c821fb539f3d338b1d150d
Andrea <valentea78@gmail.com>
2021-11-03 20:25:54 +0000

    started work on package and funs

---
+++ tools_of_trade/tester.py
@@ -0,0 +1,3 @@
+from tools_of_trade.tools import *
+
+get_latest_date()
--- tools_of_trade/tools.py
+++ tools_of_trade/tools.py
@@ -1,3 +1,6 @@
-class ToolsOfTrade:
-    pass
+def get_latest_monday() -> str:
+    return "yes"
 
+
+def get_latest_date() -> str:
+    return "no"
