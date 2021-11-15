991d4c2bc07781653edb65ee25d6ef50bef0824f
Andrea <valentea78@gmail.com>
2021-11-15 19:27:59 +0000

    started work on fuzzy matcher

--- fuzzy_matcher/fuzzy_tools.py
+++ fuzzy_matcher/fuzzy_tools.py
@@ -1,5 +1,22 @@
 # we want a function to prepare the column (replace strings with '', remove alphanumeric, lower case)
 # we then want to wrap the fuzzy matching in a function
 # we also want some quick way to rejoin the fuzzy matching dataset to the master df, or use to filter it
-def col_normalizer():
-    pass
+import pandas as pd
+
+
+class FuzzyMatcher(object):
+    def __init__(self, column1: pd.Series, column2: pd.Series, matcher: str, first_char: bool) -> None:
+        self.matcher = matcher
+        self.column1 = column1
+        self.column2 = column2
+        self.first_chars = first_char
+
+
+
+
+
+
+
+
+
+
