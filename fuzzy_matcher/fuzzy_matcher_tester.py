67c6a82d4d75204419139b013bccae56dc7d9994
Andrea <valentea78@gmail.com>
2021-11-17 16:36:42 +0000

    fixed few things on fuzzy matcher

--- fuzzy_matcher/fuzzy_matcher_tester.py
+++ fuzzy_matcher/fuzzy_matcher_tester.py
@@ -7,16 +7,14 @@ zip_list = zip(list1, list2)
 
 df = pd.DataFrame(zip_list, columns=['column1', 'column2'])
 
-fuzzy_match = FuzzyMatcher(normalization=True, column1=df.column1, column2=df.column2, matcher='fuzz.ratio',
-                           first_chars=False)
+fuzzy_match = FuzzyMatcher(normalization=True,
+                           column1=df.column1,
+                           column2=df.column2,
+                           matcher='fuzz.ratio',
+                           first_chars=2)
 
 substrings = ['ltd', 'limited']
 
-new_df = fuzzy_match.name_normalizer(sreplace=True, replace_list=substrings)  # we already initialized the cols, so
-# we should not have to pass them to this method
+new_df = fuzzy_match.name_normalizer(sreplace=True, replace_list=substrings)  # these arguments must be optional
 
-# post processor keep highest similarity score when multiple matches
-
-# option to pass a list with strings to remove
-
-# vectorised version
+res = fuzzy_match.fuzzy_matcher(threshold=3)
--- fuzzy_matcher/fuzzy_tools.py
+++ fuzzy_matcher/fuzzy_tools.py
@@ -2,14 +2,14 @@
 # we then want to wrap the fuzzy matching in a function
 # we also want some quick way to rejoin the fuzzy matching dataset to the master df, or use to filter it
 from datetime import datetime
-
+from typing import Optional
 import pandas as pd
 from fuzzywuzzy import fuzz
 
 
 class FuzzyMatcher(object):
     def __init__(self, normalization: bool, column1: pd.Series, column2: pd.Series, matcher: str,
-                 first_chars: bool) -> None:
+                 first_chars: Optional[int, None]) -> None:
         self.normalization: normalization
         self.matcher = matcher
         self.column1 = column1
@@ -31,19 +31,24 @@ class FuzzyMatcher(object):
         zipped_list = zip(self.column1, self.column2)
         return pd.DataFrame(zipped_list, columns=['col1', 'col2'])
 
-    def list_maker(self, column1: pd.Series, columns2: pd.Series):
-        pass
-
-    def fuzzy_matcher(self, first_chars: bool):
+    def fuzzy_matcher(self, threshold: int) -> pd.DataFrame:  # leave threshold optional
         fuzzy = []
         count = 0
-        print('Started at: ' + str(datetime.now().strftime('%H:%M:%S')))
+        print('Matching started at: ' + str(datetime.now().strftime('%H:%M:%S')))
         for i in self.column1:
             count += 1
             if (count % 1000) == 0:  # pass to param
                 print(str(count) + ' records matched at ' + str(datetime.now().strftime('%H:%M:%S')))
             for j in self.column2:
-                if i[:2] == j[:2]:  # pass to param
-                    fuzzy.append([i, j, fuzz.ratio(i, j)])  # we want pass the self.matcher
+                if self.first_chars is not None:
+                    if i[:self.first_chars] == j[:self.first_chars]:  # pass to param
+                        fuzzy.append([i, j, fuzz.ratio(i, j)])
+                else:
+                    fuzzy.append([i, j, fuzz.ratio(i, j)])
+            # we want pass the self.matcher
+        print('Matching completed at: ' + str(datetime.now().strftime('%H:%M:%S')))
         name_matching = pd.DataFrame(fuzzy, columns=['col1', 'col2', 'similarity_score'])
         return name_matching
+
+    def post_processor(self):
+        pass
