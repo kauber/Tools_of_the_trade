8103eed33f43f204a6d33dbbd3ae151af8b3201f
Andrea <valentea78@gmail.com>
2021-11-20 12:02:22 +0000

    minor changes

--- fuzzy_matcher/fuzzy_tools.py
+++ fuzzy_matcher/fuzzy_tools.py
@@ -2,7 +2,6 @@
 # we then want to wrap the fuzzy matching in a function
 # we also want some quick way to rejoin the fuzzy matching dataset to the master df, or use to filter it
 from datetime import datetime
-from typing import Optional, Tuple
 import pandas as pd
 from fuzzywuzzy import fuzz
 
